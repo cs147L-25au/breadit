8ab878064149eba8606ad66b5cd1a468ce4bd3ce
Emmy Thamakaison <93239069+emmyxth@users.noreply.github.com>
2025-12-04 20:12:10 -0800

    auth token persist

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,57 @@
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import { supabase } from "../lib/supabase";
+
+import type { AuthChangeEvent, Session } from "@supabase/supabase-js";
 
 export default function Index() {
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getSession = async () => {
+      try {
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+        setSession(session);
+      } catch (error) {
+        console.error("Error getting session:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getSession();
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(
+      (_event: AuthChangeEvent, session: Session | null) => {
+        setSession(session);
+        setIsLoading(false);
+      }
+    );
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (isLoading) {
+    return <View style={styles.container} />;
+  }
+
+  if (session) {
+    return <Redirect href="/(tabs)" />;
+  }
+
   return <Redirect href="/(auth)/login" />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { Database } from './database.types';
 
 const supabaseUrl = 'https://hunanacuomwfwmbdluxb.supabase.co'
 const supabaseAnonKey = 'sb_publishable_Qvn5aHCCE8C6B4RH9OBqBg_aUrM8Xrg';
 
-export const supabase = createClient<Database>(
-  supabaseUrl, supabaseAnonKey
-);
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
